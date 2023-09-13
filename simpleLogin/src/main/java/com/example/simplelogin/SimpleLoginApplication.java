4401219ee00f43b457eff5ea2867d4f244237475
Adrian Li <36882230+ariel055132@users.noreply.github.com>
2023-09-13 23:26:13 +0800

    Add the logging properties and test the log function

--- simpleLogin/src/main/java/com/example/simplelogin/SimpleLoginApplication.java
+++ simpleLogin/src/main/java/com/example/simplelogin/SimpleLoginApplication.java
@@ -1,13 +1,16 @@
 package com.example.simplelogin;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class SimpleLoginApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SimpleLoginApplication.class, args);
+        log.info("Start application");
     }
 
 }
