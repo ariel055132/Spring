1744afa3c8c8ba3682e5731091d6261ae95c79d0
Adrian Li <36882230+ariel055132@users.noreply.github.com>
2023-09-28 00:32:26 +0800

    Add Comments to Program to raise readability

--- simpleLogin/src/main/java/com/example/simplelogin/entity/User.java
+++ simpleLogin/src/main/java/com/example/simplelogin/entity/User.java
@@ -6,22 +6,26 @@ import jakarta.validation.constraints.Size;
 import lombok.Data;
 import java.io.Serializable;
 
+/**
+ * 設定用戶的帳密(Username, Password)，並利用Spring Validation設定驗證軌則
+ * NotEmpty, Size
+ */
 @Data
 @JsonIgnoreProperties(value = {"password"}, allowSetters = true)
 public class User implements Serializable {
     /**
-     * User ID
+     * User ID (用戶ID)
      */
     private Integer id;
 
     /**
-     * User name
+     * User name (用戶名字)
      */
     @NotEmpty(message = "Username cannot be null!")
     private String username;
 
     /**
-     * Password
+     * Password (用戶密碼)
      */
     @NotEmpty(message = "Password cannot be null!")
     @Size(min = 8, message = "Password length cannot shorter than 8.")
--- simpleLogin/src/main/java/com/example/simplelogin/model/Result.java
+++ simpleLogin/src/main/java/com/example/simplelogin/model/Result.java
@@ -4,21 +4,25 @@ import lombok.Data;
 
 import java.io.Serializable;
 
+/**
+ * 封裝回傳Response的顯示內容
+ */
 @Data
 public class Result<T> implements Serializable {
 
-    // Messages
+    // 消息結果
     private String message;
 
-    // Login status? (Success/Fail)
+    // 是次操作是否成功
     private boolean success;
 
+    // 消息內容
     private T data;
 
     /**
-     * Login success
-     *
-     * @param msg
+     * Success to set up, return the message only
+     * 設定結果為成功，只回傳結果
+     * @param msg 消息結果
      */
     public void setResultSuccess(String msg) {
         this.message = msg;
@@ -27,10 +31,10 @@ public class Result<T> implements Serializable {
     }
 
     /**
-     * Login success (Overloading)
-     *
-     * @param msg
-     * @param data
+     * Success to set up, return the message and the data (~function overloading)
+     * 設定結果為成功，回傳結果及相關數據/訊息
+     * @param msg 消息結果
+     * @param data 消息數據
      */
     public void setResultSuccess(String msg, T data) {
         this.message = msg;
@@ -39,9 +43,9 @@ public class Result<T> implements Serializable {
     }
 
     /**
-     * Login Fail
-     *
-     * @param msg
+     * Failed to set up
+     * 設定結果為失敗
+     * @param msg 消息結果
      */
     public void setResultFailed(String msg) {
         this.message = msg;
