037e44dd1876c79367e31dc8fdad3d4a70e7c262
Adrian Li <36882230+ariel055132@users.noreply.github.com>
2023-10-03 22:35:48 +0800

    Add comments to the program

--- simpleLogin/src/main/java/com/example/simplelogin/dao/UserDAO.java
+++ simpleLogin/src/main/java/com/example/simplelogin/dao/UserDAO.java
@@ -3,39 +3,44 @@ package com.example.simplelogin.dao;
 import com.example.simplelogin.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 
+/**
+ * DAO主要負責JAVA對DB操作的介面(Interface)和介面實現(Implementation)
+ * 介面主要實現三個功能：新增用戶，修改用戶資料，(根據用戶ID或名稱)獲取相關用戶
+ * MyBatis只要定義Interface即可
+ */
 @Mapper
 public interface UserDAO {
 
     /**
      * Create User
-     *
-     * @param user
-     * @return
+     * 新增用戶
+     * @param user 用戶實例(instance)
+     * @return Number of records added successfully (可成功新增之用戶數量)
      */
     int add(User user);
 
 
     /**
      * Edit the user message
-     *
-     * @param user
-     * @return
+     * 修改用戶資料
+     * @param user 需修改之用戶實例
+     * @return Number of records updated successfully (可成功修改資料之用戶數量)
      */
     int update(User user);
 
     /**
      * Get the User with id
-     *
-     * @param id
-     * @return User
+     * 根據用戶輸入之ID，獲取相關用戶
+     * @param id 用戶Id
+     * @return User 用戶實例(instance)
      */
     User getById(Integer id);
 
     /**
      * Get the User with username
-     *
-     * @param username
-     * @return User
+     * 根據用戶名稱，獲取相關用戶
+     * @param username 用戶名稱
+     * @return User 用戶實例(instance)
      */
     User getByUsername(String username);
 }
